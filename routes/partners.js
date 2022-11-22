b09ab7a0f46d8d1ccd316b06d70d1162fcd8ab89
Lu-Family <l.pochon@lafamily.ch>
2022-11-22 15:14:58 +0100

    initial Partner routes

---
+++ routes/partners.js
@@ -0,0 +1,84 @@
+const express = require("express");
+const router = express.Router();
+const Partner = require("../models/partner");
+
+// All partners Route
+router.get("/", async(req, res) => {
+  let searchOptions = {}
+  if(req.query.name != null && req.query !== '') {
+    searchOptions.name = new RegExp(req.query.name, 'i')
+  }
+    try{
+        const partners = await Partner.find(searchOptions)
+        res.render("partners/index", {
+          partners: partners, 
+          searchOptions: req.query
+        });
+    }catch{
+        res.redirect('/')
+    }
+
+});
+
+// New partner Route
+router.get("/new",  (req, res) => {
+  res.render("partners/new", { partner: new Partner() });
+});
+// Async fonction to deal with mangoDB and try to catch an error
+router.post("/", async (req, res) => {
+  const partner = new Partner({
+    name: req.body.name,
+  })
+  try {
+    const newPartner = await partner.save()
+   // res.redirect(`partners/${newPartners.id}`)
+    res.redirect('partners')
+  } catch {
+    res.render("partners/new", {
+      partner: partner,
+      errorMessage: "Erreur lors de la création du partenaire",
+    })
+  }
+/*
+// Autocomplete search 
+  router.get("/", async (req, res) => {
+    try {
+      let results;
+      if (req.query.name) {
+        results = await Partner.aggregate([
+          {
+            $search: {
+              index: "autocomplete",
+              autocomplete: {
+                query: req.query.name,
+                path: "name",
+                fuzzy: {
+                  maxEdits: 1,
+                },
+                tokenOrder: "sequential",
+              },
+            },
+          },
+          {
+            $project: {
+              name: 1,
+              _id: 1,
+            },
+          },
+          {
+            $limit: 10,
+          },
+        ]);
+        if (results) return res.send(results);
+      }
+      res.send([]);
+    } catch (error) {
+      console.log(error);
+      res.send([]);
+    }
+  });
+*/
+
+})
+
+module.exports = router;
--- server.js
+++ server.js
@@ -5,8 +5,11 @@ if(process.env.NODE_ENV !== 'production') {
 const express = require('express')
 const app = express()
 const expressLayouts = require('express-ejs-layouts')
+const bodyParser = require('body-parser')
+//const cors = require('cors')
 
 const indexRouter = require('./routes/index')
+const partnerRouter = require('./routes/partners')
 
 
 app.set('view engine', 'ejs')
@@ -15,18 +18,20 @@ app.set('layout', 'layouts/layout')
 
 
 app.use(expressLayouts)
+app.use(express.json())
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({limit:'10mb', extended:false}))
+//app.use(cors())
 
 const mongoose = require('mongoose')
-mongoose.connect(process.env.DATABASE_URL, { 
-    useNewUrlParser:true 
-})
+mongoose.connect(process.env.DATABASE_URL)
 
 const db = mongoose.connection
 db.on('error', error => console.error(error))
 db.once('open', () => console.log('Connected to Mongoose'))
 
 app.use('/', indexRouter)
+app.use('/partners', partnerRouter)
 
 
 app.listen(process.env.PORT || 3000) 
